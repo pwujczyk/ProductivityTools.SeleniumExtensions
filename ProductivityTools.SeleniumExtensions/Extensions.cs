4867820388148f7ecc45439a1de0e4468ac4b019
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-07-22 18:35:41 +0200

    initial commit

---
+++ ProductivityTools.SeleniumExtensions/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.ObjectModel;
+
+namespace ProductivityTools.SeleniumExtensions
+{
+    public static class Extensions
+    {
+        public static IWebElement GetElementByInnerText(this IWebElement parent, string tag, string text)
+        {
+            var tags = parent.FindElements(By.TagName(tag));
+            foreach (var item in tags)
+            {
+                var x = item.GetAttribute("innerHTML");
+                if (x == text)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+    }
+}
